0753892260b1b2be046642d27ae86bc83044bb21
lilith-cash <79391692+lilith-cash@users.noreply.github.com>
2021-03-04 20:25:45 +0800

    Create LlcUsdtLPTokenLlsPool.sol

---
+++ contracts/v2/LlcUsdtLPTokenLlsPool.sol
@@ -0,0 +1,160 @@
+pragma solidity ^0.6.0;
+
+import '../math.sol';
+import '../SafeMath.sol';
+import '../IERC20.sol';
+import '../Address.sol';
+import '../SafeERC20.sol';
+import '../interface/IRewardDistributionRecipient.sol';
+import '../LPTokenWrapper.sol';
+
+contract LlcUsdtLPTokenLlsPool is
+    LPTokenWrapper,
+    IRewardDistributionRecipient
+{
+    IERC20 public lls;
+    uint256 public constant DURATION = 30 days;
+
+    uint256 public initreward = 166319955000000000000000; // 166319.955 lls
+    uint256 public starttime; // starttime TBD
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    constructor(
+        address lls_,
+        address lptoken_,
+        uint256 starttime_
+    ) public {
+        lls = IERC20(lls_);
+        lpt = IERC20(lptoken_);
+        starttime = starttime_;
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply() == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable()
+                    .sub(lastUpdateTime)
+                    .mul(rewardRate)
+                    .mul(1e18)
+                    .div(totalSupply())
+            );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return
+            balanceOf(account)
+                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+                .div(1e18)
+                .add(rewards[account]);
+    }
+
+    // stake visibility is public as overriding LPTokenWrapper's stake() function
+    function stake(uint256 amount)
+        public
+        override
+        updateReward(msg.sender)
+        checkhalve
+        checkStart
+    {
+        require(amount > 0, 'Cannot stake 0');
+        super.stake(amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount)
+        public
+        override
+        updateReward(msg.sender)
+        checkhalve
+        checkStart
+    {
+        require(amount > 0, 'Cannot withdraw 0');
+        super.withdraw(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() external {
+        withdraw(balanceOf(msg.sender));
+        getReward();
+    }
+
+    function getReward() public updateReward(msg.sender) checkhalve checkStart {
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            lls.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    modifier checkhalve() {
+        if (block.timestamp >= periodFinish) {
+            initreward = initreward.mul(75).div(100);
+
+            rewardRate = initreward.div(DURATION);
+            periodFinish = block.timestamp.add(DURATION);
+            emit RewardAdded(initreward);
+        }
+        _;
+    }
+
+    modifier checkStart() {
+        require(block.timestamp >= starttime, 'not start');
+        _;
+    }
+
+    function notifyRewardAmount(uint256 reward)
+        external
+        override
+        onlyRewardDistribution
+        updateReward(address(0))
+    {
+        if (block.timestamp > starttime) {
+            if (block.timestamp >= periodFinish) {
+                rewardRate = reward.div(DURATION);
+            } else {
+                uint256 remaining = periodFinish.sub(block.timestamp);
+                uint256 leftover = remaining.mul(rewardRate);
+                rewardRate = reward.add(leftover).div(DURATION);
+            }
+            lastUpdateTime = block.timestamp;
+            periodFinish = block.timestamp.add(DURATION);
+            emit RewardAdded(reward);
+        } else {
+            rewardRate = initreward.div(DURATION);
+            lastUpdateTime = starttime;
+            periodFinish = starttime.add(DURATION);
+            emit RewardAdded(reward);
+        }
+    }
+
+    
+}
