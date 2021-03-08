46983806a96bcdfaa4a94bd297d13935fb023d6f
lilith-cash <79391692+lilith-cash@users.noreply.github.com>
2021-03-08 11:32:09 +0800

    Update LLCHTPool.sol

--- contracts/v2/LLCHTPool.sol
+++ contracts/v2/LLCHTPool.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.6.0;
 
 
-import '../math.sol';
-import '../SafeMath.sol';
-import '../IERC20.sol';
-import '../Address.sol';
-import '../SafeERC20.sol';
-import '../interface/IRewardDistributionRecipient.sol';
+import './math.sol';
+import './SafeMath.sol';
+import './IERC20.sol';
+import './Address.sol';
+import './SafeERC20.sol';
+import './IRewardDistributionRecipient.sol';
 
 contract HTWrapper {
     using SafeMath for uint256;
